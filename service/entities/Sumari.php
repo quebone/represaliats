c5c01030825553114c010e76c87af91bfad520dc
Carles <carles_cc@uoc.edu>
2019-07-26 07:28:39 +0200

    expanded observacions length to 2048

--- service/entities/Sumari.php
+++ service/entities/Sumari.php
@@ -49,7 +49,7 @@ class Sumari implements IEntity
 	private $hasExecucio;
 	/** @OneToOne(targetEntity="SumariExecucio", cascade="remove") **/
 	private $execucio;
-	/** @Column(type="string", length=1024, nullable=true) **/
+	/** @Column(type="string", length=2048, nullable=true) **/
 	private $observacions;
 	
 	public function __construct() {
